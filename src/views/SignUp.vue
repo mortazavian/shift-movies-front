bbd1feba4d2a24e73d77f7ca11a3922d65691264
Mehdi Mortazavian <mehdi.mortazavian@gmail.com>
2022-08-17 17:00:16 +0430

    Sing up added to the project

---
+++ src/views/SignUp.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <form action="">
+      <input class="name" type="text" v-bind="name" />
+      <input class="user-name" type="text" v-bind="userName" />
+      <input class="email" type="text" v-bind="email" />
+      <input class="password" type="text" v-bind="password" />
+
+      <button @click="signUp">Sign Up</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+  data() {
+    return {
+      currentUser: {
+        name: "",
+        userName: "",
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    async signUp() {
+      const { data, status } = await axios.post(
+        "http://localhost:8080/signup",
+        this.currentUser
+      );
+      console.log(data);
+      console.log(status);
+    },
+  },
+};
+
+// name - user name - email - password
+</script>
+
+<style scoped></style>
