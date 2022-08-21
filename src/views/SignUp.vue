6d46dd5a73a0251fc520062379fb9e1fcad7e140
Mehdi Mortazavian <mehdi.mortazavian@gmail.com>
2022-08-21 16:15:44 +0430

    sign up axios request modified

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <!-- <form> -->
+    <h1>{{ kir }}</h1>
     <label>Name</label>
     <input class="name form-control" type="text" v-model="currentUser.name" />
     <label>User Id</label>
@@ -18,14 +19,17 @@
 
     <button @click="signUp">signUp</button>
     <!-- </form> -->
+    <button @click="kiri"></button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "SignUp",
   data() {
     return {
+      kir: 0,
       currentUser: {
         name: "",
         userName: "",
@@ -35,12 +39,36 @@ export default {
     };
   },
   methods: {
-    async signUp() {
-      console.log(this.currentUser.userName);
+    signUp() {
+      //   fetch(
+      //     "https://vue-http-demo-a5b7d-default-rtdb.firebaseio.com/surveys.json",
+      //     {
+      //       method: "POST",
+      //       headers: {
+      //         "Content-type": "application/json",
+      //       },
+      //       body: JSON.stringify({
+      //         name: this.name,
+      //         userName: this.userName,
+      //         email: this.email,
+      //         password: this.password,
+      //       }),
+      //     }
+      //   );
 
-      console.log(this.currentUser.password);
+      axios.post(
+        "https://vue-http-demo-a3c0d-default-rtdb.firebaseio.com/signup.json",
+        {
+          name: this.currentUser.name,
+          userName: this.currentUser.userName,
+          email: this.currentUser.email,
+          password: this.currentUser.password,
+        }
+      );
 
-      console.log(this.currentUser.email);
+      // console.log(this.currentUser.userName);
+      // console.log(this.currentUser.password);
+      // console.log(this.currentUser.email);
       //   this.$router.push("/");
     },
   },
