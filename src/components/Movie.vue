398af1d1e9ec8aa9443df21cce6dcbc526e5b8f4
Mehdi Mortazavian <mehdi.mortazavian@gmail.com>
2022-08-23 16:55:13 +0430

    add the order of the information of a movie component

---
+++ src/components/Movie.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- name
+    
+    
+    summery 
+    zhanr
+    direcor
+    actors
+    score
+    country
+    year of publication
+    
+    -->
+  </div>
+</template>
+
+<script></script>
+<style scoped></style>
