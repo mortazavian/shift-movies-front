c2cf5ca8b7fa9ec12a55cb82978c2dfde4c6cd0d
Mehdi Mortazavian <mehdi.mortazavian@gmail.com>
2022-08-23 16:49:15 +0430

    added the axios request for home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p>{{ movies }}</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
+  data() {
+    return {
+      movies: {},
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  created() {
+    axios({
+      method: "get",
+      url: "http://127.0.0.1:8000/home/",
+    }).then((response) => {
+      this.movies = response;
+    });
+    // .catch((error) => console.log("KIRI"));
+  },
+};
 </script>
