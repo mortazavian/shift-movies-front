cad6c622e526f271788773978bb9432b9b42a9ab
Mehdi Mortazavian <mehdi.mortazavian@gmail.com>
2022-08-23 17:06:52 +0430

    add created life cycle to bind the input information to output we want

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
     <!-- name
-    
-    
     summery 
     zhanr
     direcor
@@ -10,10 +8,43 @@
     score
     country
     year of publication
-    
     -->
+    <h2 class="name">{{}}</h2>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      information: {},
+      name: "",
+      summery: "",
+      genre: "",
+      director: "",
+      actors: [],
+      score: 0,
+      country: "",
+      yearOfPublication: "",
+    };
+  },
+  methods: {
+    assignActors() {
+      for (actor in this.information.actors) {
+        this.actors.push(actor);
+      }
+    },
+  },
+  created() {
+    this.name = this.information.name;
+    this.summery = this.information.summery;
+    this.genre = this.information.genre;
+    this.director = this.information.director;
+    this.assignActors();
+    this.score = this.information.score;
+    this.country = this.information.country;
+    this.yearOfPublication = this.information.yearOfPublication;
+  },
+};
+</script>
 <style scoped></style>
