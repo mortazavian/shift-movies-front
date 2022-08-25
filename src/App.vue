d260ade0a16eb097345cb9ed425d842010d0cab9
aliiiimaher <alimohammadmaher@gmail.com>
2022-08-25 16:20:10 +0430

    nav bar added

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,19 @@
 <template>
+  <div>
+    <Menubar :model="items">
+      <template #start>
+        <img
+          alt="logo"
+          src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png"
+          height="40"
+          class="mr-2"
+        />
+      </template>
+      <template #end>
+        <InputText placeholder="Search" type="text" />
+      </template>
+    </Menubar>
+  </div>
   <nav>
     <router-link to="/signup">Sign-Up</router-link> |
     <router-link to="/">Home</router-link> |
@@ -8,6 +23,112 @@
   </nav>
   <router-view />
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          label: "Home",
+          icon: "pi pi-fw pi-file",
+          url: ""
+        },
+        {
+          label: "Edit",
+          icon: "pi pi-fw pi-pencil",
+          items: [
+            {
+              label: "Left",
+              icon: "pi pi-fw pi-align-left",
+            },
+            {
+              label: "Right",
+              icon: "pi pi-fw pi-align-right",
+            },
+            {
+              label: "Center",
+              icon: "pi pi-fw pi-align-center",
+            },
+            {
+              label: "Justify",
+              icon: "pi pi-fw pi-align-justify",
+            },
+          ],
+        },
+        {
+          label: "Users",
+          icon: "pi pi-fw pi-user",
+          items: [
+            {
+              label: "New",
+              icon: "pi pi-fw pi-user-plus",
+            },
+            {
+              label: "Delete",
+              icon: "pi pi-fw pi-user-minus",
+            },
+            {
+              label: "Search",
+              icon: "pi pi-fw pi-users",
+              items: [
+                {
+                  label: "Filter",
+                  icon: "pi pi-fw pi-filter",
+                  items: [
+                    {
+                      label: "Print",
+                      icon: "pi pi-fw pi-print",
+                    },
+                  ],
+                },
+                {
+                  icon: "pi pi-fw pi-bars",
+                  label: "List",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          label: "Events",
+          icon: "pi pi-fw pi-calendar",
+          items: [
+            {
+              label: "Edit",
+              icon: "pi pi-fw pi-pencil",
+              items: [
+                {
+                  label: "Save",
+                  icon: "pi pi-fw pi-calendar-plus",
+                },
+                {
+                  label: "Delete",
+                  icon: "pi pi-fw pi-calendar-minus",
+                },
+              ],
+            },
+            {
+              label: "Archieve",
+              icon: "pi pi-fw pi-calendar-times",
+              items: [
+                {
+                  label: "Remove",
+                  icon: "pi pi-fw pi-calendar-minus",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          label: "Quit",
+          icon: "pi pi-fw pi-power-off",
+        },
+      ],
+    };
+  },
+};
+</script>
 
 <style>
 /* #app {
