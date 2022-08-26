f52b23c71b87623ca9a8786b38cf254b9f4e19bc
aliiiimaher <alimohammadmaher@gmail.com>
2022-08-26 15:23:35 +0430

    action page set

---
+++ src/views/genre/Action.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1>This is Action Page</h1>
+    <div class="home">
+    <div class="movies" v-for="(movie, index) in movies" :key="index">
+      <Movie :information="movies[index]" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Movie from "@/components/Movie.vue";
+import axios from "axios";
+
+export default {
+  name: "Action",
+  data() {
+    return {
+      movies: [],
+    };
+  },
+  components: {
+    Movie,
+  },
+  created() {
+    axios({
+      method: "get",
+      url: "http://localhost:8000/category/action/",
+    }).then((response) => {
+      this.movies = response.data;
+    });
+  },
+};
+</script>
