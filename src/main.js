05695410c84c04736f1584d563d3a1471f2a6843
Mehdi Mortazavian <mehdi.mortazavian@gmail.com>
2022-08-26 15:18:50 +0430

    add drum page

--- src/main.js
+++ src/main.js
@@ -1,27 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import PrimeVue from 'primevue/config';
-import router from './router'
-import store from './store/index';
+import { createApp } from "vue";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import router from "./router";
+import store from "./store/index";
 
-// - - - - - - - PrimeVue Importing - - - - - - - - 
-import "../node_modules/primeflex/primeflex.css"            //flex
-import "primevue/resources/themes/saga-blue/theme.css"      //theme
-import "primevue/resources/primevue.min.css"                //core css
-import "primeicons/primeicons.css"                          //icons
-import Menubar from 'primevue/menubar';                     //menubar
-import Button from "primevue/button";                       //button
-import Dialog from 'primevue/dialog';                       //dialog
-import InputText from 'primevue/inputtext';                 //inputText
+// - - - - - - - PrimeVue Importing - - - - - - - -
+import "../node_modules/primeflex/primeflex.css"; //flex
+import "primevue/resources/themes/saga-blue/theme.css"; //theme
+import "primevue/resources/primevue.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
+import Menubar from "primevue/menubar"; //menubar
+import Button from "primevue/button"; //button
+import Dialog from "primevue/dialog"; //dialog
+import InputText from "primevue/inputtext"; //inputText
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.component('Dialog', Dialog);
-app.component('Button', Button);
-app.component('Menubar', Menubar);
-app.component('InputText', InputText);
+app.component("Dialog", Dialog);
+app.component("Button", Button);
+app.component("Menubar", Menubar);
+app.component("InputText", InputText);
 
 app.use(PrimeVue);
-app.use(store).use(router)
+app.use(store).use(router);
 
-app.mount('#app')
+app.mount("#app");
