728d62f2ff2fb3e8935d233d2e7d6224169d0b6d
Mehdi Mortazavian <mehdi.mortazavian@gmail.com>
2022-08-28 12:06:31 +0430

    add sigle movie commponent and send request inside it

--- src/views/userMethods/SingleMovie.vue
+++ src/views/userMethods/SingleMovie.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
-    <h1>{{ id }}kir</h1>
+    <!-- <h1>{{ id }}kir</h1> -->
     <!-- <h1>kir kochooloo</h1> -->
+
+    <SingleMovieComponentVue :information="info" />
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import SingleMovieComponentVue from "@/components/SingleMovieComponent.vue";
 export default {
+  components: {
+    SingleMovieComponentVue,
+  },
   data() {
     return {
       id: this.$route.query,
+      info: {},
     };
   },
   created() {
@@ -18,6 +25,8 @@ export default {
       method: "get",
       url: "http://127.0.0.1:8000/film/" + this.id.id + "/",
     }).then((response) => {
+      this.info = response.data;
+      console.log(this.info);
       console.log(response);
     });
   },
