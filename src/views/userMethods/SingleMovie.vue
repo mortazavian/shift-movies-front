81279bd7e8f39ff8708953f154398f08a79b60fb
Mehdi Mortazavian <mehdi.mortazavian@gmail.com>
2022-08-28 11:53:55 +0430

    can send and resive response with axios

--- src/views/userMethods/SingleMovie.vue
+++ src/views/userMethods/SingleMovie.vue
@@ -6,11 +6,20 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       id: this.$route.query,
     };
+  },
+  created() {
+    axios({
+      method: "get",
+      url: "http://127.0.0.1:8000/film/" + this.id.id + "/",
+    }).then((response) => {
+      console.log(response);
+    });
   },
 };
 </script>
