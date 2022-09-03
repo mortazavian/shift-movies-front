6f85bc2c6e06d9a49be8078ac71d57fc5e8b4806
aliiiimaher <alimohammadmaher@gmail.com>
2022-09-03 22:40:46 +0430

    MsnzmT

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,7 +3,7 @@
     <h1>This is an about page</h1>
     <hr />
     <h2>Ali Maher: <a href="https://github.com/aliiimaher">aliiimaher</a></h2>
-    <h2>Mohsen Zahmatkesh: <a href="https://github.com/MsnzmT">MsnZmt</a></h2>
+    <h2>Mohsen Zahmatkesh: <a href="https://github.com/MsnzmT">MsnzmT</a></h2>
     <h2>Mehdi Mortazavian: <a href="https://github.com/mortazavian">mortazavian</a></h2>
   </div>
 </template>
